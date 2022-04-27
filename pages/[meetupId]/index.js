8eaef8452d547a230c6e1d6169ffe098da5dd6b5
himoura32 <fallingout31@gmail.com>
2022-04-27 06:38:49 -0400

    coded programmatic nav/cust components

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail.js";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/3/3b/Frauenkirche_and_Neues_Rathaus_Munich_March_2013.JPG"
+      title="First Meetup"
+      address="54 Munich Street"
+      desc="This is the first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
